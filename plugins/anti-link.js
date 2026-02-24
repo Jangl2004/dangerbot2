706e6a4d92616967051a582c27adaf5960792263
dthbot <blebledarius@gmail.com>
2026-02-24 19:43:18 +0100

    Update anti-link.js

--- plugins/anti-link.js
+++ plugins/anti-link.js
@@ -1,5 +1,5 @@
 
-import { downloadContentFromMessage } from '@realvare/based'
+import { downloadContentFromMessage } from '@realvare/baileys'
 import ffmpeg from 'fluent-ffmpeg'
 import { createWriteStream, readFile } from 'fs'
 import { join } from 'path'
