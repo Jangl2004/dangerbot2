dacd4a084d352deb2bee008aadf418655f29c778
Jangl2004 <alessandromagno722@gmail.com>
2026-03-06 11:26:50 +0100

    Update playmp4.js
    Signed-off-by: Jangl2004 <alessandromagno722@gmail.com>

--- plugins/playmp4.js
+++ plugins/playmp4.js
@@ -6,17 +6,23 @@ if(!text) return conn.reply(m.chat,'❌ URL mancante',m)
 
 await conn.reply(m.chat,'🎬 Scarico video...',m)
 
-let api = `https://api.akuari.my.id/downloader/youtube?link=${text}`
+try {
 
-let res = await fetch(api)
-let json = await res.json()
+let res = await fetch(`https://api.siputzx.my.id/api/d/ytmp4?url=${text}`)
+let data = await res.json()
 
-let video = json.mp4
+let video = data.data.dl
 
 await conn.sendMessage(m.chat,{
 video:{url:video},
 caption:'🎬 Ecco il tuo video'
 },{quoted:m})
+
+} catch(e){
+
+conn.reply(m.chat,'❌ Errore download video',m)
+
+}
 
 }
 
