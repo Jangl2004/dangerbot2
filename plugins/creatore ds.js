18c85d32dada92da894bf5b32bc98fcf22dff665
Jangl2004 <alessandromagno722@gmail.com>
2026-03-10 06:55:31 +0100

    Update creatore ds.js
    Signed-off-by: Jangl2004 <alessandromagno722@gmail.com>

--- plugins/creatore ds.js
+++ plugins/creatore ds.js
@@ -1,50 +1,13 @@
 import fs from 'fs'
-import path from 'path'
 
 let handler = async (m, { conn }) => {
-    // Lista delle cartelle sessioni più comuni nei bot Baileys/VareBot
-    const possibiliCartelle = ['./session', './sessions', './auth_info_baileys', './auth_info', './sessione']
+    // Leggiamo cosa c'è nella cartella principale (dove si trova handler.js)
+    const cartelle = fs.readdirSync('./').filter(f => fs.statSync('./' + f).isDirectory());
     
-    // Trova la prima cartella esistente nella lista
-    let sessionPath = possibiliCartelle.find(p => fs.existsSync(p))
-
-    if (!sessionPath) {
-        return m.reply('❌ Errore: Non ho trovato nessuna cartella di sessione (ho cercato session, sessions, auth_info_baileys). Controlla il nome della cartella nel tuo file manager.')
-    }
-
-    try {
-        const files = await fs.promises.readdir(sessionPath)
-        let deletedCount = 0
-
-        for (const file of files) {
-            // Fondamentale: NON eliminare creds.json o il bot si disconnette
-            if (file !== 'creds.json') {
-                const filePath = path.join(sessionPath, file)
-                const stat = await fs.promises.stat(filePath)
-                
-                // Elimina solo i file, non le sottocartelle
-                if (stat.isFile()) {
-                    await fs.promises.unlink(filePath)
-                    deletedCount++
-                }
-            }
-        }
-
-        if (deletedCount === 0) {
-            return m.reply(`✨ La cartella *${sessionPath}* è già pulita! Non c'erano file inutili da rimuovere.`)
-        }
-
-        m.reply(`✅ Pulizia completata nella cartella: *${sessionPath}*\n\nEliminati **${deletedCount}** file di sessione "sporchi".\nIl file *creds.json* è stato salvato.`)
-        
-    } catch (err) {
-        console.error(err)
-        m.reply('❌ Si è verificato un errore durante la pulizia dei file.')
-    }
+    m.reply('📂 Cartelle trovate nella directory principale:\n\n' + cartelle.join('\n'));
 }
 
-handler.help = ['ds']
-handler.tags = ['owner']
-handler.command = /^(ds|cleansession)$/i
-handler.owner = true // Solo il proprietario può usarlo
+handler.command = /^(debugfolders)$/i
+handler.owner = true
 
 export default handler
