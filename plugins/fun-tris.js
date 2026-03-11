b236a8da49c6dd85753b6dc8045a3f2e84a82ce4
Jangl2004 <alessandromagno722@gmail.com>
2026-03-11 20:13:32 +0100

    Update fun-tris.js
    Signed-off-by: Jangl2004 <alessandromagno722@gmail.com>

--- plugins/fun-tris.js
+++ plugins/fun-tris.js
@@ -1,47 +1,34 @@
-import { createCanvas, loadImage } from 'canvas';
+class TrisGame:
+    def __init__(self):
+        self.board = [" " for _ in range(9)]
+        self.turn = "X"  # X inizia sempre
+        self.game_over = False
 
-// --- DICHIARAZIONI GLOBALI PER PERSISTENZA E VISIBILITÀ ---
-global.games = global.games || new Map();
-global.timeoutMap = global.timeoutMap || new Map();
-global.playerStats = global.playerStats || new Map();
+    def check_winner(self):
+        win_coords = [(0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6)]
+        for a, b, c in win_coords:
+            if self.board[a] == self.board[b] == self.board[c] and self.board[a] != " ":
+                return self.board[a]
+        if " " not in self.board: return "Pareggio"
+        return None
 
-// --- CLASSE PRINCIPALE DEL GIOCO TRIS ---
-class TrisGame {
-    constructor(p1, p2) {
-        this.p1 = p1;
-        this.p2 = p2;
-        this.board = Array(9).fill(null);
-        this.turn = p1;
-        this.isFinished = false;
-        this.winningLine = null;
-    }
+    def get_board_text(self):
+        b = self.board
+        return f"```\n {b[0]} | {b[1]} | {b[2]} \n---+---+---\n {b[3]} | {b[4]} | {b[5]} \n---+---+---\n {b[6]} | {b[7]} | {b[8]} \n```"
 
-    move(pos) {
-        if (pos < 0 || pos > 8 || this.board[pos] || this.isFinished) {
-            return false;
-        }
-        this.board[pos] = this.turn === this.p1 ? 'X' : 'O';
-        this.turn = this.turn === this.p1 ? this.p2 : this.p1;
-        return true;
-    }
-
-    checkWin() {
-        const winPatterns = [
-            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Righe
-            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Colonne
-            [0, 4, 8], [2, 4, 6]             // Diagonali
-        ];
-        for (let pattern of winPatterns) {
-            const [a, b, c] = pattern;
-            if (this.board[a] && this.board[a] === this.board[b] && this.board[a] === this.board[c]) {
-                return { winner: this.board[a], line: pattern };
-            }
-        }
-        if (!this.board.includes(null)) {
-            return { winner: 'draw', line: null };
-        }
-        return null;
-    }
-}
-
-// --- FUN
+# Esempio di utilizzo nel tuo bot
+def handle_move(game, position):
+    if game.game_over: return "La partita è finita! Scrivi .tris per ricominciare."
+    
+    # Esegue la mossa
+    game.board[position] = game.turn
+    
+    # Controlla vittoria
+    winner = game.check_winner()
+    if winner:
+        game.game_over = True
+        return f"🎉 Partita finita! Risultato: {winner if winner != 'Pareggio' else 'Pareggio'}"
+    
+    # Cambia turno
+    game.turn = "O" if game.turn == "X" else "X"
+    return f"Tocca a: {game.turn}\n{game.get_board_text()}"
