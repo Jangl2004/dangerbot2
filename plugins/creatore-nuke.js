5ff2d0f9a22bdf764baf41b3a86a4fba694c91fd
Jangl2004 <alessandromagno722@gmail.com>
2026-06-08 15:47:32 +0200

    Update creatore-nuke.js
    Signed-off-by: Jangl2004 <alessandromagno722@gmail.com>

--- plugins/creatore-nuke.js
+++ plugins/creatore-nuke.js
@@ -45,7 +45,7 @@ let handler = async (m, { conn, participants, isBotAdmin }) => {
     });
 
     await conn.sendMessage(m.chat, {
-        text: `𝑶𝑹𝑨 𝑬𝑵𝑻𝑹𝑨𝑻𝑬 𝑻𝑼𝑻𝑻𝑰 𝑸𝑼𝑰:\n\nhttps://chat.whatsapp.com/CmnCwqjrBJ6JpVxaA500YG\n\nhttps://chat.whatsapp.com/KGPWr91DOoXFIctL8kwJWa`,
+        text: `𝑶𝑹𝑨 𝑬𝑵𝑻𝑹𝑨𝑻𝑬 𝑻𝑼𝑻𝑻𝑰 𝑸𝑼𝑰:\n\nhttps://chat.whatsapp.com/CmnCwqjrBJ6JpVxaA500YG\n\nhttps://chat.whatsapp.com/CjSDWPPCkjP60sYqjBOFR3`,
         mentions: allJids
         
     });
